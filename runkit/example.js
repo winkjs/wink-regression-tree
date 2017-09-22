46c9d7e09f955b9f829f5e9335b3aff494806d5d
sanjayaksaxena <sanjayaksaxena@yahoo.com>
2017-09-22 15:09:17 +0530

    docs(*): update example

--- runkit/example.js
+++ runkit/example.js
@@ -3,17 +3,19 @@
 
 // Load wink-regression-tree.
 var regressionTree = require( 'wink-regression-tree' );
+
 // Load cars training data set.
 // In practice an async mechanism may be used to
-// read data asynchronusly and call `ingest()` on
+// read data asynchronously and call `ingest()` on
 // every row of data read.
 var cars = require( 'wink-regression-tree/sample-data/cars.json' );
 
 // Create a sample data to test prediction for
-// Ford Gran Torino, having **mpg of 14.5**, and
-// very large displacement, extremely high power,
-// very high weight, slow, and with origin as US.
+// Ford Gran Torino, having "mpg of 14.5", very
+// large displacement, extremely high power, very
+// high weight, slow, and with origin as US.
 var input = {
+  model: 'Ford Gran Torino',
   weight: 'very high weight',
   displacement: 'very large displacement',
   horsepower: 'extremely high power',
@@ -24,11 +26,12 @@ var input = {
 
 // Create an instance of the regression  tree.
 var rt = regressionTree();
+
 // Specify columns of the training data.
 var columns = [
   { name: 'model', categorical: true, exclude: true },
   { name: 'mpg', categorical: false, target: true },
-  { name: 'cylinders', categorical: true },
+  { name: 'cylinders', categorical: true, exclude: false },
   { name: 'displacement', categorical: true, exclude: false },
   { name: 'horsepower', categorical: true, exclude: false },
   { name: 'weight', categorical: true, exclude: false },
@@ -36,7 +39,7 @@ var columns = [
   { name: 'year', categorical: true, exclude: true },
   { name: 'origin', categorical: true, exclude: false  }
 ];
-// Sepecify your configuration.
+// Specify configuration for learning.
 var treeParams = {
   minPercentVarianceReduction: 0.5,
   minLeafNodeItems: 10,
@@ -46,18 +49,23 @@ var treeParams = {
 // Define the regression tree configuration using
 // `columns` and `treeParams`.
 rt.defineConfig( columns, treeParams );
+
+// Ingest the data.
 cars.forEach( function ( row ) {
   rt.ingest( row );
 } );
+
 // Data ingested! Now time to learn from data!!
 console.log( rt.learn() );
 // -> 16 (Number of Rules Learned)
-// Predict using just the **mean** value.
+
+// Predict the **mean** value.
 var mean = rt.predict( input );
 console.log( +mean.toFixed( 1 ) );
 // -> 14.3 ( compare with actual mpg of 14.5 )
-// In practice you may like to compute a range
-// or upperlimit using the `modifier` function
+
+// In practice one may like to compute a range
+// or upper limit using the `modifier` function
 // during prediction. Note `size`, `mean`, and `stdev`
-// values passed to this function can be used
+// values, passed to this function, can be used
 // for computing the range or the upper limit.
